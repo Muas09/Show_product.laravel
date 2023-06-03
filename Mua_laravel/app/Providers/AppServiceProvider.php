9b89b4cb2f727d6fac109caf2ee437d212b85136
Muas09 <116275467+Muas09@users.noreply.github.com>
2023-06-03 10:57:00 +0700

    add admin

--- Mua_laravel/app/Providers/AppServiceProvider.php
+++ Mua_laravel/app/Providers/AppServiceProvider.php
@@ -3,22 +3,37 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\ProductType;
+use App\Models\Product;
+use App\Models\type_product;
+
+use function PHPSTORM_META\type;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    /**												
+	    												
+	     *												
+	     * @return void												
+	     */
+    public function register()
     {
-        //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    /**												
+	     * Bootstrap any application services.												
+	     *												
+	     * @return void												
+	     */
+    public function boot()
     {
-        //
+        view()->composer('header', function ($view) {
+            $loai_sp = type_Product::all();
+            $view->with('loai_sp', $loai_sp);
+        });
+        view()->composer('page.type_product', function ($view) {
+            $product_new = Product::where('new', 1)->orderBy('id', 'DESC')->skip(1)->take(8)->get();
+            $view->with('product_new', $product_new);
+        });
     }
 }
--- Mua_laravel/routes/web.php
+++ Mua_laravel/routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\AreaofShapeController;
-use App\Http\Controllers\SingupController;
-use App\Http\Controllers\XinchaoController;
-use App\Http\UserController\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,86 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-//27/4/2023
-Route::get('/',function(){
+Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/mas', [App\Http\Controllers\PageController::class, 'getIndex']);
+Route::get('/detail/{id}', [App\Http\Controllers\PageController::class, 'getDetail']);
 
- Route::get('/ac', [App\Http\Controllers\PageController::class, 'getIndex']);
-//  Route::get('/ac', [App\Http\Controllers\PageController::class, 'loai_sanpham']);
+Route::get('/type/{id}', [App\Http\Controllers\PageController::class, 'getLoaiSp']);
 
-use Illuminate\Support\Facades\Schema;
-Route::get('database',function(){
-    Schema::create('loaisanpham',function($table){
-        $table->increments('id');
-        $table->string('ten',200);
-    });
-    echo"Đã thực hiện lệnh tạo bảng thành công";
-});
-//22/4/2023
+Route::get('/admin', [App\Http\Controllers\PageController::class, 'getIndexAdmin']);
 
-// Route::get('/form', [SingupController::class, 'index']);
-// Route::post('/form', [SingupController::class, 'displayInFor']);
-
-// Route::get('/',function(){
-
-// })
-
-// Route ::get('/welcome',function(){
-//     return 'Chào mừng các bạn đến với PNV';
-// });
-
-// Route::get('/welcome',[XinchaoController:: class,'Hello']);
-// Route::get('/', [XinchaoController::class, 'Hello']);
-// 
-
-// Route::get('/computeArea', [AreaofShapeController::class, 'computeArea']);
-// Route::post('/computeArea', [AreaofShapeController::class, 'computeArea']);
-
-// Route::get('/sum', [XinchaoController::class, 'Hello']);
-// Route::post('/sum', [XinchaoController::class, 'Hello']);
-
-// Route::get('/',function(){
-//     // $html='<h1>Học lập trình Laravel</h1>';
-//     // return 'Home page';
-    
-// });
-// Route::get('/', function  () {
-//     return view('welcome');
-// });
-
-// Route::get('/unicode',function(){
-//     return view('form');
-//     //return 'Phương thức Get của path/unicode';
-// });
-// Route::post('/unicode',function(){
-//     return 'Phương thức Post của path/unicode';
-// });
-
-// Route::put('unicode',function(){
-//     return 'Phương thức Put của path/unicode';
-// });
-
-// Route::delete('unicode',function(){
-//     return 'Phương thức Delete của path/unicode';
-// });
-
-// Route::patch('unicode',function(){
-//     return 'Phương thức Patch của path/unicode';
-// });
+Route::get('/admin-add-form', [App\Http\Controllers\PageController::class, 'getAdminAdd'])->name('add-product');
+Route::post('/admin-add-form', [App\Http\Controllers\PageController::class, 'postAdminAdd']);
+Route::get('/admin-edit-form/{id}', [App\Http\Controllers\PageController::class, 'getAdminEdit']);
+Route::post('/admin-edit', [App\Http\Controllers\PageController::class, 'postAdminEdit']);
 
 
-// //Nhận nhiều phương thức
+Route::post('/admin-delete/{id}', [App\Http\Controllers\PageController::class, 'postAdminDelete'])->name('delete-product');
 
-// Route::match(['get','post'],'unicode',function(){
-//     return $_SERVER['REQUEST_METHOD'];
-// });
-// Route::get('show-form',function(){
-//     return view('form');
-// });
 
-// Route::any('unicode',function(){
-//     return $_SERVER['REQUEST_METHOD'];
-// });
+Route::post('/admin-export', [App\Http\Controllers\PageController::class, 'exportAdminProduct'])->name('export');
