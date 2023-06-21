04d4a415f8865b736f2ff5da47f05dde81cf0b88
Muas09 <116275467+Muas09@users.noreply.github.com>
2023-06-21 16:38:10 +0700

    Update
    login and sig up and card

--- Mua_laravel/laravel/resources/views/vonglapfor.blade.php
+++ Mua_laravel/laravel/resources/views/vonglapfor.blade.php
@@ -1,3 +1,7 @@
+@for ($i = 1; $i <= 5; $i++)
+    <div class="square"></div>
+@endfor
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,21 +13,24 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 </head>
+<style>
+    .square {
+    width: 100px;
+    height: 100px;
+    background-color: red;
+    margin: 10px;
+    float: left;
+}
+</style>
 <body>
-    <form action="/tinhtong" method="post">
-        @csrf 
-        <div class="form_group">
-            <input type="number" class="form-control" placeholder="Số a" name="soA">
+<div class="square-container">
+    @for ($i = 1; $i <= 5; $i++)
+        <div class="row">
+            @for ($j = 1; $j <= 5; $j++)
+                <div class="square"></div>
+            @endfor
         </div>
-        <div class="form_group">
-            <input type="number" class="form-control" placeholder="Số B" name="soB">
-        </div>
-        <div class="form_group">
-            <button type="submit" class="btn btn-primary">Tính</button>
-        </div>
-        <div class="form_group">
-            <input type="number" class="form-control" placeholder="Kết quả" disabled="" value="<?php if(isset($sum)) echo $sum; ?>">
-        </div>
-    </form>
+    @endfor
+</div>
 </body>
 </html>
--- Mua_laravel/laravel/routes/web.php
+++ Mua_laravel/laravel/routes/web.php
@@ -34,3 +34,59 @@ Route::post('/admin-delete/{id}', [App\Http\Controllers\PageController::class, '
 
 
 Route::post('/admin-export', [App\Http\Controllers\PageController::class, 'exportAdminProduct'])->name('export');
+Route::post('/luan11/{id}', [PageController::class, 'postAdminDelete']);
+
+Route::get('/register', function () {
+    return view('users.register');
+});
+Route::post('/register', [UserController::class, 'Register']);
+
+Route::get('/login', function () {
+    return view('users.login');
+});
+Route::post('/login', [UserController::class, 'Login']);
+Route::get('/logout', [UserController::class, 'Logout']);
+
+
+
+Route::get('add-to-cart/{id}', [PageController::class, 'getAddToCart'])->name('themgiohang');
+
+Route::get('del-cart/{id}', [PageController::class, 'getDelItemCart'])->name('xoagiohang');
+
+Route::get('check-out', [PageController::class, 'getCheckout'])->name('dathang');
+Route::post('check-out', [PageController::class, 'postCheckout'])->name('dathang');
+
+
+
+Route::get('/mua4', [PageController::class, 'marter']);
+
+
+Route::get('/mua5', [PageController::class, 'if']);
+
+Route::get('/mualuan6', [PageController::class, 'for']);
+
+Route::get('/mua7', [PageController::class, 'lienhe']);
+
+Route::get('/mua8', [PageController::class, 'about']);
+
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+
+
+Route::get('/tinhtich/{a}/{b}', function ($a, $b) {
+    echo $a * $b;
+    exit;
+})->whereNumber('a')->whereNumber('b');
+Route::get('/tinhtong/{a}/{b}', function ($a, $b) {
+    echo $a + $b;
+    exit;
+})->whereNumber('a')->whereNumber('b');
+Route::get('/tinhhieu/{a}/{b}', function ($a, $b) {
+    echo $a - $b;
+    exit;
+})->whereNumber('a')->whereNumber('b');
+Route::get('/tinhthuong/{a}/{b}', function ($a, $b) {
+    echo $a / $b;
+    exit;
+})->whereNumber('a')->whereNumber('b');
